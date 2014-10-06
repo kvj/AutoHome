3ea6ae3ca45b1ac21cbe21888fbeba041803dba1
Konstantin Vorobyev <kvorobyev@gmail.com>
2014-10-07 07:19:03 +0900

    Arduino Project structure

---
+++ Client/AutoHome/AutoHome.h
@@ -0,0 +1,12 @@
+#ifndef _AUTOHOME_H_
+#define _AUTOHOME_H_
+
+#include "AutoHome_config.h"
+
+struct Sensor_Type_Info {
+	int type;
+};
+
+typedef struct Sensor_Type_Info SensorTypeInfo;
+typedef SensorTypeInfo* PSensorTypeInfo;
+#endif
---
+++ Client/AutoHome/AutoHome.ino
@@ -0,0 +1,9 @@
+#include "AutoHome.h"
+
+#include "plugin_DHT.c"
+
+void setup() {
+}
+
+void loop() {
+}
