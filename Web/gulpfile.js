129cca8847629435309645a3f04381556c5101f2
Konstantin Vorobyev <vorobyev@coacha.com>
2014-10-27 18:45:58 +0900

    Web project setup with gulp and editorconfig

---
+++ Web/gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+var concat = require('gulp-concat');
+var order = require('gulp-order');
+
+var watch = require('gulp-watch');
+var less = require('gulp-less');
+var coffee = require('gulp-coffee');
+
+var lessPath = 'static/less/**/[a-z]*.less';
+var cssPath = 'static/css/';
+var coffeePath = 'static/coffee/**/*.coffee';
+var jsPath = 'static/js/';
+
+gulp.task('less', function() {
+    return gulp
+        .src(lessPath).pipe(watch(lessPath, function(files) {
+            return files.pipe(less({
+            })).pipe(concat('app.css')).pipe(gulp.dest(cssPath));
+        }));
+});
+
+gulp.task('coffee', function() {
+    return gulp
+        .src(coffeePath).pipe(watch(coffeePath, function(files) {
+            return files.pipe(coffee({
+                bare: true
+            }).on('error', gutil.log)).pipe(concat('app.js')).pipe(gulp.dest(jsPath));
+        }));
+});
+
+gulp.task('default', ['less', 'coffee']);
