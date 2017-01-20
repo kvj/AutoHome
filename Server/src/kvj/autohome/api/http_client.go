d072f0f6148e8c45d65146eb869df3475254b567
Konstantin Vorobyev <vorobyev@coacha.com>
2017-01-20 10:29:48 +0900

    Simplify tmux helper

--- Server/src/kvj/autohome/api/http_client.go
+++ Server/src/kvj/autohome/api/http_client.go
@@ -18,20 +18,13 @@ type sensorDef [4]int
 var sensors = map[string]sensorDef{
 	"room_t":      {2, 0, 0, 1},
 	"room_0_move": {2, 1, 0, 0},
-	// "room_0_light": {2, 2, 0, 0},
 	"room_1_move": {3, 1, 0, 0},
-	// "room_1_light": {3, 2, 0, 0},
 	"room_2_move": {0, 1, 0, 0},
-	// "room_2_light": {0, 2, 0, 0},
 	"room_3_move": {1, 1, 0, 0},
-	// "room_3_light": {1, 2, 0, 0},
-	"out_cond": {10, 20, 0, 0},
-	"out_t":    {10, 20, 0, 1},
-	"out_wind": {10, 20, 0, 5},
-	"out_wsp":  {10, 20, 0, 6},
-	"batt_0":   {30, 3, 0, 0},
-	"batt_1":   {35, 3, 0, 0},
-	"batt_2":   {40, 3, 0, 0},
+	"out_cond":    {10, 20, 0, 0},
+	"out_t":       {10, 20, 0, 1},
+	"out_wind":    {10, 20, 0, 5},
+	"out_wsp":     {10, 20, 0, 6},
 }
 
 func url(conf data.HashMap, uri string) string {
@@ -109,23 +102,16 @@ func MakeStatus(conf data.HashMap) (string, string) {
 		// log.Printf("Error:", err)
 		return "", "HTTP error"
 	}
-	outp := fmt.Sprintf("%s %s %s%.f : %s : %s%s%s%s : %.f%%/%.f%%/%.f%%",
+	outp := fmt.Sprintf("%s %s %s%.f : %s : %s%s%s%s",
 		internet.Code2Char(vals["out_cond"]),
 		internet.FormatTemp(vals["out_t"]),
 		internet.FormatWindDirection(vals["out_wind"]),
 		vals["out_wsp"],
 		internet.FormatTemp(vals["room_t"]),
 		makeFlag(vals["room_0_move"]),
-		// makeBar(256-vals["room_0_light"], 256),
 		makeFlag(vals["room_1_move"]),
-		// makeBar(256-vals["room_1_light"], 256),
 		makeFlag(vals["room_2_move"]),
-		// makeBar(256-vals["room_2_light"], 256),
-		makeFlag(vals["room_3_move"]),
-		// makeBar(256-vals["room_3_light"], 256),
-		vals["batt_0"],
-		vals["batt_1"],
-		vals["batt_2"])
+		makeFlag(vals["room_3_move"]))
 	// log.Printf("Data: %v %s", vals, outp)
 	return outp, ""
 }
